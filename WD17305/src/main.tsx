115cdc0c4d3822069871281ef9a491de0667619e
hoanganh203 <105597730+hoanganh203@users.noreply.github.com>
2023-07-11 15:17:12 +0700

    Component

--- WD17305/src/main.tsx
+++ WD17305/src/main.tsx
@@ -2,10 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import CounterProvider from './useContext/index.tsx'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <CounterProvider>
+  <React.StrictMode>
     <App />
-  </CounterProvider>,
+  </React.StrictMode>,
 )
